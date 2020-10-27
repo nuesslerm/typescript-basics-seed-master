49cadfad5ce3dc3c2ba999b9e2cbc32950262bed
nuesslerm <markus@choco.com>
2020-10-27 23:02:54 +0100

    es6/7 and typescript

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
-/* const pizzas = [{ name: 'Pepperoni', toppings: ['pepperoni'] }];
+/* 
+* arrow functions and implicit returns
+
+const pizzas = [{ name: 'Pepperoni', toppings: ['pepperoni'] }];
 
 // const mappedPizzas = pizzas.map(function (pizza) {
 //   return pizza.name.toUpperCase();
@@ -24,12 +27,96 @@ const pizza = {
   },
 };
 
+* default function params
+
 console.log(pizza.getName());
  */
 
-function multiply(a: number, b: number = 25) {
-  return a * b;
+// function multiply(a: number, b: number = 25) {
+//   return a * b;
+// }
+
+// console.log(multiply(5));
+// console.log(multiply(5, 35));
+
+// * object literal improvements
+
+// const pizza = {
+//   name: 'Pepperoni',
+//   price: 15,
+//   getName() {
+//     return this.name;
+//   },
+// };
+
+// console.log(pizza.getName());
+
+// const toppings = ['pepperoni'];
+
+// const order = {
+//   pizza,
+//   toppings,
+// };
+
+// function createOrder(pizza: String, toppings: String) {
+//   return { pizza, toppings };
+// }
+
+// console.log(order);
+
+// * rest param
+
+// function sumAll(message: String, ...arr: number[]): number {
+//   console.log(arguments[0]);
+//   return arr.reduce((acc, curr) => acc + curr, 0);
+// }
+
+// const sum = sumAll('Hello', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
+
+// console.log(sum);
+
+// * array spread operator
+
+// const toppings = ['bacon', 'chilli'];
+
+// const newToppings = ['pepperoni'];
+
+// const allToppings = [...toppings, ...newToppings];
+
+// console.log(allToppings);
+
+// * object spread operator
+
+// const pizza = {
+//   name: 'pepperoni',
+// };
+
+// const toppings = ['pepperoni'];
+
+// // const order = {
+// //   ...pizza,
+// //   toppings,
+// // };
+
+// // const finalOrder = { ...order };
+
+// // console.log(finalOrder);
+
+// const order = Object.assign({}, pizza, { toppings });
+
+// const spreadOrder = { ...pizza, toppings };
+
+// console.log('%s\n%s', order, spreadOrder);
+
+// * destructuring arrays and objects
+
+const pizza = {
+  name: 'Pepperoni',
+  toppings: ['pepperoni'],
+};
+
+function order({ name, toppings }: { name: String; toppings: String[] }) {
+  console.log(name, toppings);
 }
 
-console.log(multiply(5));
-console.log(multiply(5, 35));
+order(pizza);
